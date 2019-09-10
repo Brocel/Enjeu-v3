3de7b7a103977488267573a488daa09b9e6f7e27
Brocel <geenens.prof@gmail.com>
2019-09-10 19:41:36 +0200

    importer le core uniquement dans app.module2

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,19 +2,17 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublicModule } from '../public/public.module';
 import { ProtectedModule } from '../protected/protected.module';
-import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [FooterComponent],
+  declarations: [],
   imports: [
     CommonModule,
     PublicModule,
     ProtectedModule
   ],
   exports: [
-    FooterComponent
   ]
 })
 export class CoreModule {
