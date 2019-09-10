474908a44edc8e9fd2c47299b7b582ce7699578a
Brocel <geenens.prof@gmail.com>
2019-09-10 19:35:36 +0200

    importer le core uniquement dans app.module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublicModule } from '../public/public.module';
 import { ProtectedModule } from '../protected/protected.module';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [FooterComponent],
   imports: [
     CommonModule,
     PublicModule,
     ProtectedModule
+  ],
+  exports: [
+    FooterComponent
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+  //Importer le core uniquement dans app.module
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+   if (parentModule) {
+    throw new Error('CoreModule is already loaded.');
+   }
+  }
+}
