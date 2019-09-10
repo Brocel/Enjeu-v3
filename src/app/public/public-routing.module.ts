38e5b4b8463fad7f6508c246e3fa7718659b6fea
Brocel <geenens.prof@gmail.com>
2019-09-10 20:07:02 +0200

    route vers home

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent }, // on fait correspondre la route home au composant home
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
