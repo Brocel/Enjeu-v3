cbf836ae6f0e251f47b2cfc543925c91145eb9ae
Brocel <geenens.prof@gmail.com>
2019-09-11 17:55:54 +0200

    edit personnage

--- src/app/shared/models/personnage.ts
+++ src/app/shared/models/personnage.ts
@@ -2,6 +2,7 @@ export class Personnage {
   // Présentation
   readonly id: string; //id personnage
   role: Array<'JOUEUR'|'MOB'>; // rôles : joueur ou ennemi
+  readonly titre: string;
   readonly niveau: number; //niveau du perso
   name: string;
   avatar: string; // url de la photo
@@ -16,11 +17,32 @@ export class Personnage {
   taille: number; // en cm
   masse: number; // en kg
   age: number; // en années
-  readonly for: number; //force physique du personnage
-  readonly def: number; //résistance physique/endurance du personnage
-  readonly int: number; //force/résistance mentale / charisme du personnage
+  for: number; //force physique du personnage
+  def: number; //résistance physique/endurance du personnage
+  int: number; //force/résistance mentale / charisme du personnage
   // Autres
   inventaire: string; //objets portés
   readonly exp: number; // expérience du personnage
+
+  constructor(
+    id: string,
+    role: Array<'JOUEUR'|'MOB'>,
+    name: string,
+    avatar: string, // url de la photo
+    sexe: string,
+    race: string,
+    metier: string,
+    specialite: string,
+    taille: number,
+    masse: number,
+    age: number
+  )
+  {
+    this.exp = 0;
+    if (this.exp==0) {
+      this.titre = 'Nouvel arrivant';
+      this.niveau = 1;
+    }
+  }
 
 }
