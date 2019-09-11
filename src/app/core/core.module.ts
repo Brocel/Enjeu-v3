e5136c4c2307f25ddc7962b56992222549bdc36a
Brocel <geenens.prof@gmail.com>
2019-09-11 16:27:42 +0200

    imports de modules et services dans core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,34 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { FormsModule } from  '@angular/forms';
+import { ReactiveFormsModule } from  '@angular/forms';
+import { HttpClientModule } from  '@angular/common/http';
+
 import { SharedModule } from 'src/app/shared/shared.module';
 import { PublicModule } from '../public/public.module';
 import { ProtectedModule } from '../protected/protected.module';
 
-
+import { AuthService } from './services/auth.service';
+import { AuthGuardService } from './services/auth-guard.service';
+import { MdjService } from './services/mdj.service';
+import { PersonnagesService } from './services/personnages.service';
 
 @NgModule({
   declarations: [],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     SharedModule,
     PublicModule,
     ProtectedModule
   ],
   exports: [
+  ],
+  providers: [
+    AuthService,
+    AuthGuardService,
+    MdjService,
+    PersonnagesService
   ]
 })
 export class CoreModule {
