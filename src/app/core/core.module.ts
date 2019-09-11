e44ea93036192a2e56bc63c29ed31c0cd711ca76
Brocel <geenens.prof@gmail.com>
2019-09-11 17:26:23 +0200

    ajout de firebase2

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -12,6 +12,8 @@ import { AuthGuardService } from './services/auth-guard.service';
 import { MdjService } from './services/mdj.service';
 import { PersonnagesService } from './services/personnages.service';
 
+import * as firebase from 'firebase';
+
 @NgModule({
   declarations: [],
   imports: [
@@ -36,6 +38,19 @@ export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
    if (parentModule) {
     throw new Error('CoreModule is already loaded.');
-   }
+  } else {
+      // Your web app's Firebase configuration
+    var firebaseConfig = {
+      apiKey: "AIzaSyD9fkuj-4-V9B3Qe2c3TpU5LKIAK8NxkWY",
+      authDomain: "enjeu-v3.firebaseapp.com",
+      databaseURL: "https://enjeu-v3.firebaseio.com",
+      projectId: "enjeu-v3",
+      storageBucket: "",
+      messagingSenderId: "616273300231",
+      appId: "1:616273300231:web:9eae8df262841fe8e0d3a2"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
   }
+}
 }
