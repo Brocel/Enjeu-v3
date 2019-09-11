2b90db76d4232a76272335cac26a3a0b3a3d60fb
Brocel <geenens.prof@gmail.com>
2019-09-11 18:24:13 +0200

    edit auth service ajout de methodes de connexion firebase

--- src/app/shared/models/user.ts
+++ src/app/shared/models/user.ts
@@ -1,10 +1,9 @@
 export class User {
-  readonly id: string; // id de l'utilisateur
   email: string; // email de l'utilisateur
-  isNewUser: boolean; // première connexion de l'utilisateur ?
-  lastLoginDate: number; // date de la dernière connexion
+  password: string; //mdp utilisateur
   name: string; // nom de l'utilisateur
   avatar: string; // url de la photo de profil
-  registrationDate: number; // date d'inscription
-  role: Array<'JOUEUR'|'MDJ'>; // rôles
+  constructor(email: string, password: string) {
+
+  }
 }
